4ece99ceed3a46b3aa1bf50a5dab4b8b3e7f1f02
Angel Bardon <bardon.angel@gmail.com>
2023-04-18 18:49:21 +0200

    insert_into

--- m1_caso_practico.sql
+++ m1_caso_practico.sql
@@ -15,7 +15,7 @@ create table libros (
 	titulo varchar (250),
 	editorial varchar (100),
     category varchar (50),
-    precio decimal (3, 2),
+    precio decimal (12, 2),
     id_autor int,
     foreign key (id_autor) references autores (id_autor)
 );
@@ -35,21 +35,32 @@ create table prestamos (
     id_usuario int,
     fecha_inicio date,
     fecha_fin date,
-    recargo decimal(2, 2) default 0.0,
+    recargo decimal(12, 2) default 0.0,
     foreign key (id_libro) references libros(id_libro),
     foreign key (id_usuario) references usuarios(id_usuario)
 ); 
 
 select * from autores;
 select * from libros;
-select * from usuario;
+select * from usuarios;
 select * from prestamos;
 
-    
+INSERT INTO `m1_caso_practico`.`autores` (`nombre`, `apellidos`, `fecha_nacimiento`) VALUES ('francisco', 'mora', '1952-07-28');
+INSERT INTO `m1_caso_practico`.`autores` (`nombre`, `apellidos`, `fecha_nacimiento`) VALUES ('angel', 'limon', '1942-09-12');
+INSERT INTO `m1_caso_practico`.`autores` (`nombre`, `apellidos`, `fecha_nacimiento`) VALUES ('pablo', 'garcia', '1969-05-15');
+INSERT INTO `m1_caso_practico`.`autores` (`nombre`, `apellidos`, `fecha_nacimiento`) VALUES ('maria', 'olmo', '1999-07-01');
+INSERT INTO `m1_caso_practico`.`autores` (`nombre`, `apellidos`, `fecha_nacimiento`) VALUES ('dolores', 'casas', '2000-12-12');
 
+INSERT INTO `m1_caso_practico`.`libros` (`titulo`, `editorial`, `category`, `precio`, `id_autor`) VALUES ('pesadilla', 'planeta', 'novela', '19.20', '2');
+INSERT INTO `m1_caso_practico`.`libros` (`titulo`, `editorial`, `category`, `precio`, `id_autor`) VALUES ('paraiso', 'everest', 'novela', '18.00', '1');
+INSERT INTO `m1_caso_practico`.`libros` (`titulo`, `editorial`, `category`, `precio`, `id_autor`) VALUES ('ingles facil', 'planeta', 'texto', '9.00', '5');
+INSERT INTO `m1_caso_practico`.`libros` (`titulo`, `editorial`, `category`, `precio`, `id_autor`) VALUES ('el futuro es hoy', 'santillana', 'teatro', '25.75', '2');
 
+INSERT INTO `m1_caso_practico`.`usuarios` (`nif`, `email`, `fecha_alta`, `codigo_postal`) VALUES ('78276421s', 'maria@gmail.com', '202-05-14', '28000');
+INSERT INTO `m1_caso_practico`.`usuarios` (`nif`, `email`, `fecha_alta`, `codigo_postal`) VALUES ('55667665a', 'felipe@hot.com', '2023-01-02', '28002');
+INSERT INTO `m1_caso_practico`.`usuarios` (`nif`, `email`, `fecha_alta`, `codigo_postal`) VALUES ('76887231c', 'teresa@gmail.com', '2019-05-02', '28002');
+INSERT INTO `m1_caso_practico`.`usuarios` (`nif`, `email`, `fecha_alta`, `codigo_postal`) VALUES ('36227889h', 'sofia@gmail.com', '2020-02-02', '28002');
 
-
-
-
-
+INSERT INTO `m1_caso_practico`.`prestamos` (`id_libro`, `id_usuario`, `fecha_inicio`, `fecha_fin`, `recargo`) VALUES ('3', '1', '2023-03-10', '2023-04-15', '2.50');
+INSERT INTO `m1_caso_practico`.`prestamos` (`id_libro`, `id_usuario`, `fecha_inicio`, `fecha_fin`, `recargo`) VALUES ('2', '3', '2023-04-01', '2023-04-10', '0.00');
+INSERT INTO `m1_caso_practico`.`prestamos` (`id_libro`, `id_usuario`, `fecha_inicio`, `fecha_fin`, `recargo`) VALUES ('2', '4', '2023-04-07', '2023-04-17', '0.00');
