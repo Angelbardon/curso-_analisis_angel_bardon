181c18d0a89f6ae74c7a0a0efd10286f7610b9ac
Angel Bardon <bardon.angel@gmail.com>
2023-04-19 20:03:40 +0200

    operadores

---
+++ 1.operadores.py
@@ -0,0 +1,15 @@
+'''
+Estructura de datos: tuplas
+
+'''
+# fila de una tabla
+employee1 = (1, 'juan', 'garcia', 'juan@gmail.com', 30, False)
+employee2 = (2, 'patricia', 'garcia', 'patricia@gmail.com', 40, True)
+
+table_employees = [
+    employee1 = (1, 'juan', 'garcia', 'juan@gmail.com', 30, False)
+    employee2 = (2, 'patricia', 'garcia', 'patricia@gmail.com', 40, True) 
+]
+
+
+# 
---
+++ 4.diccionarios.py
@@ -0,0 +1,14 @@
+
+
+alumnos ={
+    '11111111A': 'Juan garcia',
+    '22222222B': 'usuario1 usuario1',
+    '33333333C': 'usuario2 usuario2',
+    '44444444D': 'usuario3 usuario3',  
+}
+
+# diccionario con tuplas
+tabla_alumnos = {
+    1: ('Juan', 'Apellido', 'juan@gmail.com', 30),
+    2: ('Usuario2', 'Apellido2', 'user2@gmail.com',27),
+}
