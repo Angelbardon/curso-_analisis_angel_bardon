a4c044aa804f4d7214c96ebe9a13decee3e98eaf
Angel Bardon <bardon.angel@gmail.com>
2023-04-19 19:11:05 +0200

    listas

--- m2-python/1.variables.py
+++ m2-python/1.variables.py
@@ -1,1 +1,22 @@
+''' 
+Creación de variables y asignacion 
+int
+float
+complex
+str
+bool
+
+'''
 print("Hola")
+
+
+nombre = "Angel"
+apellido = "Bardon"
+full_name = "Angel bardon"
+edad = 54
+
+print(nombre + " " + apellido)
+
+print('Hola, soy ' + nombre + ' ', 'mi edad es: '+ str('edad'))
+
+print('Hola, soy ', nombre, 'mi edad es', edad)
---
+++ m2-python/listas.py
@@ -0,0 +1,29 @@
+numero = 0
+numeros = [1, 2, 3, 4, 5]
+
+print(numero)
+print(numeros)
+
+nifs = ['25525552y', '4666366j', '74774623z']
+print(nifs)
+
+validos = [True, False, True, False]
+print(validos)
+
+lista_heterogenera = [1, 'texto', True, 29.9]
+print(lista_heterogenera)
+
+precios = [9.99, 23.43, 54.56]
+print(precios)
+
+emails = ['user1@gmail.com', 'user2@gmail.com', 'user3@gmail.com']
+telefonos = ['776667666', '999777666', '747874463']
+
+contactos =[
+    emails,
+    telefonos
+]
+
+print(emails[0])
+print(contactos)
+print(contactos)
