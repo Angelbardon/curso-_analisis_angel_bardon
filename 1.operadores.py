d0c1e7d5fe51673dafcd4c51f93672b13ac2e12d
Angel Bardon <bardon.angel@gmail.com>
2023-04-19 20:20:59 +0200

    cambio

--- 1.operadores.py
+++ 1.operadores.py
@@ -7,9 +7,17 @@ employee1 = (1, 'juan', 'garcia', 'juan@gmail.com', 30, False)
 employee2 = (2, 'patricia', 'garcia', 'patricia@gmail.com', 40, True)
 
 table_employees = [
-    employee1 = (1, 'juan', 'garcia', 'juan@gmail.com', 30, False)
-    employee2 = (2, 'patricia', 'garcia', 'patricia@gmail.com', 40, True) 
+employee1 = (1, 'juan', 'garcia', 'juan@gmail.com', 30, False)
+employee2 = (2, 'patricia', 'garcia', 'patricia@gmail.com', 40, True) 
 ]
 
+edad = 30
 
-# 
+edad = edad + 5
+print(edad)
+
+edad += 5
+print(edad)
+
+edades = [ 18, 19, 20, 21, 22, 23]
+print(18 in edades)
