27dd5d1417cdc304775521afa97576e62af3dd06
Angel Bardon <bardon.angel@gmail.com>
2023-04-20 16:53:50 +0200

    if.py

---
+++ for.py
@@ -0,0 +1,12 @@
+'''
+Estructura de control iterarivo o de repeticion
+
+bucle for es ñuñna estructura determinada
+'''
+
+names =['juan', 'malala', 'rosa'] # estructura de datos siempre en plural
+
+print('rosa' in names)
+
+for name in names:
+    print(name)
---
+++ if.py
@@ -0,0 +1,29 @@
+
+
+edad = 17
+
+if edad >= 18:
+    print('Es mayor de edad')
+else:
+    print('Es menor de edad')
+     
+    # if - else normal
+    
+altura = 180
+if altura >= 160 and altura <= 170:
+    print ('grupo1')
+elif altura > 170 and altura <= 180:
+    print('gurpo2')
+else:
+    print('grupo3')
+    
+      # if- else operador ternario
+      
+print('Es mayor de edad ') if edad >= 18 else print('Es menor de edad')
+      
+      
+      # if elif else pera multiples casos
+
+altura = 180
+if altura >= 160 and altura <= 170:
+          
