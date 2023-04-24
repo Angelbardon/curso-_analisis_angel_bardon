1cdcff4b51d9ec37e79bc610f61d145dec7d29db
Angel Bardon <bardon.angel@gmail.com>
2023-04-24 16:19:18 +0200

    func_param_defecto

---
+++ funciones_param_defecto.py
@@ -0,0 +1,10 @@
+
+
+def saludar1(nombre, edad):
+    pass
+def saludar2(nombre = 'Juan', edad = 30):
+    print(f" hola soy {nombre} con edad {edad}")
+    
+saludar2()
+saludar2("Antonio")  
+saludar2 ("Albert, 40")
