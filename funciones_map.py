a0300bc4460ae500e35c7635dafa53b9ef753859
Angel Bardon <bardon.angel@gmail.com>
2023-04-24 18:51:53 +0200

    map_landa

--- funciones_map.py
+++ funciones_map.py
@@ -10,7 +10,7 @@ for price in prices:
 print(prices_iva)
     
  
- 
+ # Funcion lamda
   
 prices = [10, 20, 30, 40, 50]
 
@@ -20,3 +20,14 @@ resultado_total = list (map(sumar_el_iva, prices))
 
 print (resultado_total)
 
+# Funcion Def
+
+def sumar_iv(price):
+    return price * 1.21
+
+print(f"El resultado es: {list(map(sumar_iva, prices))}")
+
+# Funcion map con lamda
+
+sumariva2 = lambda price: price * 1.21
+print(f"El resultado con lambda es: {list(map(sumariva2, prices))}")
