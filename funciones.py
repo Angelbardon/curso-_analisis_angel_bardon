d82d02778f4f4a41522cfa439055c58980386fe1
Angel Bardon <bardon.angel@gmail.com>
2023-04-24 15:45:45 +0200

    funciones

---
+++ funciones.py
@@ -0,0 +1,15 @@
+def print_is_adult(age):
+    print('Es mayor de edad') if age>= 18 else print('Es menor de edad')
+    
+print_is_adult(17)
+print_is_adult(18)
+print_is_adult(19)
+print_is_adult(20)
+
+#funcion con un parametro de entrada y que devuelve bool
+
+def es_mayor_edad(age):
+    return age >= 18
+
+print(es_mayor_edd(20))
+print(es_menor_edd(15))
