3b3e17bb9e5934a3800345fee50ab5a554a53448
Angel Bardon <bardon.angel@gmail.com>
2023-04-25 15:27:53 +0200

    cambios

---
+++ m2-python/poo.p.y/ordenador.py
@@ -0,0 +1,21 @@
+
+class Ordenador:
+    
+    def __init__(self, fabricante, modelo, anio, precio):
+        self.fabricante = fabricante
+        self.modelo = modelo 
+        self. anio = anio
+        self. precio = precio
+        
+    def encender(self):
+        self.estado = True
+        
+    def aplicar_descuento(self, descuento):
+        if (descuento > 0 and descuento < 0.9)
+        self.precio = self.precio = self.precio * descuento
+        
+    ordenador1 = Ordenador("Asus", "A55A",2008, 495 )
+    ordenador1.precio *= 0.9
+    
+    
+      
