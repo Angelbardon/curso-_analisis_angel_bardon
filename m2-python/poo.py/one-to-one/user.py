7fa2ae48e7392df746ab1e23e00fdd068e0af5af
Angel Bardon <bardon.angel@gmail.com>
2023-04-25 18:43:52 +0200

    one-to-many

--- m2-python/poo.py/one-to-one/user.py
+++ m2-python/poo.py/one-to-one/user.py
@@ -8,12 +8,13 @@ import datetime
 class User:
 
     # Metodo constructor
-    def __init__(self, id, nif, brithday, email, phone, password):
+    def __init__(self, id, nif, brithday, email, phone, address, password = 'admin'):
         self.id = id
         self.nif = nif
         self.brithday = brithday
         self.email = email
         self.phone = phone
+        self.address = address # Asociacion con un objeto de otra clase
         self.password = password
 
 
@@ -32,13 +33,24 @@ class Address:
 
 address1 = Address(1, 'avd.castilla', '10', 'Madrid', '28000')
 address2 = Address(2, 'calle luna', 's/n', 'Barcelona', '80000')
+address3 = Address(3, 'calle sol', '25', 'Valencia', '35000')
 
 print(address1.street, address1.city, address1.postal_code)
 print(address2.city, address2.street, address2.postal_code)
 
 
-user1 = User(1, '52552551Z', datetime.date(1995, 1, 5), 'user1@gmail.com', '123212223', 'usuario1')
-user2 = User(2, '22222551Y', datetime.date.fromisoformat("1975-01-25"), 'user2@gmail.com', '654226676', 'usuario2')
+user1 = User(1, '52552551Z', datetime.date(1995, 1, 5), 'user1@gmail.com', '123212223')
+user2 = User(2, '22222551Y', datetime.date.fromisoformat("1975-01-25"), 'user2@gmail.com', '654226676')
+user3 = User(3, '33366633A', datetime.date(2020, 2, 2), 'user3@gmail.com', '333666335')
 
 print(user1.brithday)
 print(user2.brithday)
+
+
+user3 = User(3, '53353556G', datetime.date(2000, 1,1), 'user3@gmail.com', '666777888',address1)
+user1.address = address1
+user2.address = address2
+user3.address = address3
+
+print(user3.address.city)
+print(user3.address.street)
