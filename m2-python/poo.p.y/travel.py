ba85daabecc9a404839579a276b87b7aba6e8eff
Angel Bardon <bardon.angel@gmail.com>
2023-04-25 15:30:54 +0200

    cambios

---
+++ m2-python/poo.p.y/travel.py
@@ -0,0 +1,18 @@
+import datetime
+class Travel:
+    def __init__(self, city_from, city_to, date_from, date_to):
+        
+        self.city_from = city_from
+        self.city_to = city_to
+        self.date_from = date_from
+        self.date_to = date_to
+        
+date_from = datetime.date.today()
+date_to = datetime.date.today()
+travel1 = Travel("Madrid", "Praga", date_from, date_to)
+print(f"Dia de salida {travel1.date_from} y dia de regreso {travel1.date_to}")
+
+travel2 = Travel("Barcelona", "Budapest", datetime.date(2023, 1, 15),datetime.date(2023, 1, 16))
+print(f"Dia de salida {travel2.date_from} y dia de regreso {travel2.date_to}")
+
+travel3 = Travel ("Asturias", "Viena", datetime.datetime.today(), datetime.datetime(2023, 5, 13, 10))
