5e72be3508e180ff9985637004f341ab53768869
Angel Bardon <bardon.angel@gmail.com>
2023-04-26 17:47:03 +0200

    offer

---
+++ m2-python/poo.py/Modulos/offer.py
@@ -0,0 +1,16 @@
+
+# Crear una clase Offer qu represente oferta trabajo
+# title, company, salary, workmode, exp_years
+
+class Joboffer:
+    def __init__(self, id, title, company, salary, workmode, exp_years)
+        self.id = id
+        self.title = title
+        self.company = company
+        self.salary = salary
+        self.workmode = workmode
+        self.exp_years = exp_years
+
+    def __str__(self)
+        return f" {self.id} {self.title}" 
+        
--- m2-python/poo.py/str/book.py
+++ m2-python/poo.py/str/book.py
@@ -9,13 +9,19 @@ class Author:
         self.num_books = num_books
 
     def __str__(self):
-        return f"Author con id = {self.id}" \
-            f"nif = {self.nif} " \
-            f"email = {self.email} " \
-            f"year = {self.year} " \
-            f"city = {self.city} " \
-            f'num_books = {self.num_books}'
-
-
+        # Opcion 1
+        # return f"Author con id = {self.id}" \
+            # f"nif = {self.nif} " \
+            # f"email = {self.email} " \
+            # f"year = {self.year} " \
+            #  f"city = {self.city} " \
+            # f'num_books = {self.num_books}'
+            
+            # Opcion 2
+        return 'Author %s' % (self.nif)
+        
+            # Opcion 3
+        # return 'Author %s %s %s' % (self.id, self.nif, self.email)
+            
 author1 = Author(1, '23223223R', 'author1@email.com', 2000, 'Madrid')
 print(author1)
