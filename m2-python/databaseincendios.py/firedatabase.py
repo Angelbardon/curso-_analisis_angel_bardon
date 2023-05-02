4974f0f04f8c3055048c1f46d68ee6e81e08a4a2
Angelbardon <bardon.angel@gmail.com>
2023-05-02 10:38:30 +0200

    añadido database

--- m2-python/databaseincendios.py/firedatabase.py
+++ m2-python/databaseincendios.py/firedatabase.py
@@ -13,14 +13,16 @@ class FireDatabase:
     def find_by_active(self, active):
         for fire in self.fires:
             if fire.active == active:
-                return True
+                return fire
             
         return None    
         
     def find_by_cause(self, cause):
         for fire in self.cause:
             if fire.cause == cause:
-                return cause
+                return fire
+            
+        return None
             
             
     def find_all_by_fire(self, active, cause):
@@ -29,8 +31,45 @@ class FireDatabase:
         for fire in self.fires:
             if fire.active == active and fire.cause == cause:
                 filtered_fires.append(fire)
-                
         return filtered_fires
     
+    def save(self, fire):
+        
+        id_maximo = 0
+        for current_fire in self.travels:
+            if current_fire.id > id_maximo:
+                id_maximo = current_fire.id
+                
+
+        fire.id = id_maximo + 1
+        
+        self.fires.append(fire)
+        
+    def update(self, fire):
+        for current_fire in self.fires:
+            if current_fire.id == fire.id:
+                current_fire.street = fire.street
+                current_fire.province = fire.province
+                current_fire.level = fire.level
+                current_fire.data_from = fire.data_from
+                current_fire.data_to = fire.data_to
+                current_fire.active = fire.active
+                current_fire.cause = fire.cause
+                return True
+            
+        return False
+    
+    def delete_by_id(self, id):
+        for fire in self.fires:
+            if fire.id == id:
+                self.fires.remove(fire)
+                break
+            
+    def delete_all(self):
+        self.fires = []        
+                   
+                
+                   
+                 
                       
                      
